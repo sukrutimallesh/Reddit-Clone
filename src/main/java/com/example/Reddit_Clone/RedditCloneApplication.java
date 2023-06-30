30b231e52f3b1d04901a25269427685925da5afe
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-06-30 08:02:18 -0700

    Initial commit

---
+++ src/main/java/com/example/Reddit_Clone/RedditCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reddit_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneApplication.class, args);
+	}
+
+}
